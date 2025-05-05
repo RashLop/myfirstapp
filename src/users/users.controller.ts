573e50dbcfc52eda15b799843d747e2f46c4c684
RashLop <lopezmejiarasshid@outlook.com>
2025-05-05 19:25:21 -0400

    me voy a matar

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    findAll(){
+        return 'Mostrar todos los usuarios'; 
+    }
+
+    @Get(':id')
+    findOne(){
+        return 'Mostrar usuario con ID: ${id}'; 
+    }
+    @Post() 
+    create(@Body() body: any){
+        return{
+            message: 'Usuario creado', 
+            data: body,
+        }; 
+    }
+    
+    @Put(':id')
+    update(@Param('id')id: string, @Body() body: any){
+        return{
+            message: 'Usuario con ID ${id} actualizado', 
+            data: body, 
+        }; 
+    }
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return 'usuario con ID ${id} eliminado'; 
+    }
+    
+}
